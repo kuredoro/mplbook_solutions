9c0ee34bcdba6a11c04223c2a6679f4332c6af2b
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-01-26 10:30:03 +0300

    Exercises 4-1, 4-2: added alternative implementation

--- 4-1 logical_or logical_and/main.cpp
+++ 4-1 logical_or logical_and/main.cpp
@@ -1,4 +1,5 @@
 #include <boost/mpl/bool.hpp>
+#include <boost/mpl/if.hpp>
 
 namespace mpl = boost::mpl;
 
@@ -18,48 +19,44 @@ struct logical_or<A, B, false>
     : mpl::bool_<B::value>
 {};
 
-/***************
- * Logical And *
- ***************/
-template <class A, class B, bool vA = A::value>
-struct logical_and;
+static_assert(
+    logical_or<mpl::bool_<true>, int>::value == true
+);
 
+static_assert(
+    logical_or<mpl::bool_<false>, mpl::bool_<false>>::value == false
+);
+
+static_assert(
+    logical_or<mpl::bool_<false>, mpl::bool_<true>>::value == true
+);
+
+
+/******************************
+ * Logical And                *
+ * Alternative implementation *
+ ******************************/
 template <class A, class B>
-struct logical_and<A, B, false>
-    : mpl::bool_<false>
+struct logical_and
+    : mpl::if_<
+        A,
+        B,
+        mpl::false_
+      >::type
 {};
 
-template <class A, class B>
-struct logical_and<A, B, true>
-    : mpl::bool_<B::value>
-{};
+static_assert(
+    logical_and<mpl::bool_<false>, int>::value == false
+);
+
+static_assert(
+    logical_and<mpl::bool_<true>, mpl::bool_<false>>::value == false
+);
+
+static_assert(
+    logical_and<mpl::bool_<true>, mpl::bool_<true>>::value == true
+);
 
 int main()
 {
-    // Or
-    static_assert(
-        logical_or<mpl::bool_<true>, int>::value == true
-    );
-
-    static_assert(
-        logical_or<mpl::bool_<false>, mpl::bool_<false>>::value == false
-    );
-
-    static_assert(
-        logical_or<mpl::bool_<false>, mpl::bool_<true>>::value == true
-    );
-
-
-    // And
-    static_assert(
-        logical_and<mpl::bool_<false>, int>::value == false
-    );
-
-    static_assert(
-        logical_and<mpl::bool_<true>, mpl::bool_<false>>::value == false
-    );
-
-    static_assert(
-        logical_and<mpl::bool_<true>, mpl::bool_<true>>::value == true
-    );
 }
