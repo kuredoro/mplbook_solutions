717ad4c126a45910639503f5ae7c9059ce49492b
しずえ <cauchymoon@mail.ru>
2021-02-14 15:16:20 +0300

    Exercise 6-0

---
+++ 6-0 smallest/main.cpp
@@ -0,0 +1,51 @@
+#include <type_traits>
+#include <limits>
+#include <boost/mpl/inserter.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/copy.hpp>
+#include <boost/mpl/sizeof.hpp>
+#include <boost/mpl/min.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/size_t.hpp>
+#include <boost/mpl/greater.hpp>
+#include <boost/mpl/if.hpp>
+namespace mpl = boost::mpl;
+
+#include <boost/mpl/placeholders.hpp>
+using namespace mpl::placeholders;
+
+template <class Seq>
+using smallest =
+    mpl::copy<
+        Seq,
+        mpl::inserter<
+            typename mpl::deref<typename mpl::begin<Seq>::type>::type,
+            mpl::if_<
+                mpl::greater<
+                    mpl::sizeof_<_1>,
+                    mpl::sizeof_<_2>
+                >,
+                _2,
+                _1
+            >
+        >
+    >;
+
+using vec = mpl::vector<int[2], char, double&>;
+
+static_assert(
+    std::is_same<
+        smallest<vec>::type,
+        char
+    >(),
+    ""
+);
+
+/// Alternatively, smallest could be implemented via fold.
+/// In my vision, mpl::fold will clearly represent what this
+/// metafunction actually does
+/// without any obfuscation in the form of mpl::inserter
+
+int main()
+{
+}
