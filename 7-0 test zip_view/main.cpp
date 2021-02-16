d4c2a6e5e1e23fe6315aee8dea53f6c828422c37
しずえ <cauchymoon@mail.ru>
2021-02-16 21:34:14 +0300

    Exercise 7-0

--- 7-0 test zip_view/main.cpp
+++ 7-0 test zip_view/main.cpp
@@ -2,6 +2,8 @@
 #include <boost/mpl/transform.hpp>
 #include <boost/mpl/deref.hpp>
 #include <boost/mpl/iterator_range.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/equal.hpp>
 namespace mpl = boost::mpl;
 
 #include <boost/mpl/placeholders.hpp>
@@ -55,6 +57,64 @@ struct zip_view
         >
       >
 {};
+
+
+using seq1 = mpl::vector<char, int, long>;
+using seq2 = mpl::vector<long, short, char>;
+using merged = mpl::vector<mpl::vector<char, long>, mpl::vector<int, short>, mpl::vector<long, char>>;
+
+/*
+/// Has some wierd template instantiation error deep within the library :(
+static_assert(
+    mpl::equal<
+        zip_view<mpl::vector<seq1, seq2>>,
+        merged,
+        mpl::equal<_, _>
+    >()
+);
+*/
+
+static_assert(
+    mpl::equal<
+        mpl::deref<
+            mpl::begin<
+                zip_view<mpl::vector<seq1, seq2>>
+            >::type
+        >::type,
+        mpl::vector<char, long>
+    >(),
+    ""
+);
+
+static_assert(
+    mpl::equal<
+        mpl::deref<
+            mpl::advance<
+                mpl::begin<
+                    zip_view<mpl::vector<seq1, seq2>>
+                >::type,
+                mpl::int_<1>
+            >::type
+        >::type,
+        mpl::vector<int, short>
+    >(),
+    ""
+);
+
+static_assert(
+    mpl::equal<
+        mpl::deref<
+            mpl::advance<
+                mpl::begin<
+                    zip_view<mpl::vector<seq1, seq2>>
+                >::type,
+                mpl::int_<2>
+            >::type
+        >::type,
+        mpl::vector<long, char>
+    >(),
+    ""
+);
 
 int main()
 {
